080536306a728e31e35f798773996831705bca71
Quinn Outram <quinnoutram>
2022-10-22 19:07:29 +1100

    Typewritter effect added to landing page

--- script/script.js
+++ script/script.js
@@ -2,9 +2,25 @@ const hamburg = document.getElementById('hamburger');
 const topBun = document.getElementById('top');
 const middleBun = document.getElementById('middle');
 const bottomBun = document.getElementById('bottom');
+const typing = document.getElementById('landing');
 
 hamburg.addEventListener('click', function(){
   topBun.classList.toggle('active')
   middleBun.classList.toggle('active')
   bottomBun.classList.toggle('active')
 })
+
+let i = 0
+let text1 = "Hi! My name is Quinn."
+let text2 = "I am a Developer."
+let speed = 100;
+
+function typeWriter() {
+  if (i < text1.length) {
+    typing.innerHTML += text1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed)
+  }
+}
+
+typeWriter()
