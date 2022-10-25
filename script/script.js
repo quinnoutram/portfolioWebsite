a4126984a87a012bbb7e71f984e749d4ab145ec5
Quinn Outram <quinnoutram>
2022-10-25 16:15:00 +1100

    Completed landing page nav menu + initialised aboutMe page

--- script/script.js
+++ script/script.js
@@ -2,13 +2,15 @@ const hamburg = document.getElementById('hamburger');
 const topBun = document.getElementById('top');
 const middleBun = document.getElementById('middle');
 const bottomBun = document.getElementById('bottom');
+const header = document.getElementById('header');
 
 hamburg.addEventListener('click', function(){
   topBun.classList.toggle('active')
   middleBun.classList.toggle('active')
   bottomBun.classList.toggle('active')
+  header.classList.toggle('active')
 })
-const prompts = ['Hi, My name is Quinn.', 'I am a junior developer.'];
+const prompts = ['Hi, my name is Quinn.', 'I am a junior developer.'];
 const speed = 110;
 const pause = 2600; // <--- the longer delay between text direction changes
 
@@ -18,9 +20,9 @@ function typeWriter(i=0, index=1, direction=1) {
 
   if (typing.length >= prompts[i].length) { // reverses direction
     setTimeout(() => typeWriter(i, index-1, -1), pause);
-  } else if (typing.length === 0) { // changes the index of "texts"
+  } else if (typing.length === 0) { // changes the index of the array "texts"
     setTimeout(() => typeWriter((i+1) % prompts.length), speed);
-  } else { // continue in the current direction
+  } else { // progress in current direction
     setTimeout(() => typeWriter(i, index+direction, direction), speed);
   }
 }
